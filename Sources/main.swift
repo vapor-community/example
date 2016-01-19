0d0f22612d8df724475cd20b79ad24032169a880
Tanner Nelson <tanner@bluebite.com>
2016-01-18 23:20:50 -0500

    first commit

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,24 +1,23 @@
 import Vapor
 
+Route.get("/") { request in 
+	return "<h1>Welcome.</h1> Server powered by Swift."
+}
+
 Route.get("heartbeat") { request in
 	return ["lub": "dub"]
 }
 
-Route.resource("heartbeat2", controller: HeartbeatController())
-
-Route.get("heartbeat3", closure: HeartbeatController().index)
+Route.get("/heartbeat/alternate", closure: HeartbeatController().index)
 
 Route.get("/test") { request in 
 	return View(path: "index.html")
-}
-
-Route.get("/") { request in 
-	return "<h1>Welcome.</h1> Server powered by Swift. <pre>\(request.method) \(request.parameters)</pre>"
 }
 
 Route.get("param/:thing") { request in 
 	return "<h1>param test</h1> <pre>Method: \(request.method)\nParams: \(request.parameters)\nQuery: \(request.query)</pre>"	
 }
 
+//start the server up
 let server = HttpServer()
 server.run(port: 8080)
