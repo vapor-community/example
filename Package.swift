79329333073e6e3811a7fa5f1d2c95fedfbf3a9d
Tanner <tannernelson@gmail.com>
2016-01-21 10:25:37 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "MyApp",
     dependencies: [
-        .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 1),
-		//.Package(url: "../vapor", majorVersion: 1),
+        .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 1)
     ]
 )
