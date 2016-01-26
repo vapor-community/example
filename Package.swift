2024154fa44b848e3b4790d4cfa58fcefdff91e7
Marcus Kida <marcus@kida.io>
2016-01-26 18:28:51 +1100

    Fix version number

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 1)
+        .Package(url: "https://github.com/tannernelson/vapor.git", minorVersion: 1)
     ]
 )
