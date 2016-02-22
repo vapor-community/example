1beb9d18f56e13f67e5214067949b28d11fff2e0
shnhrrsn <shnhrrsn@gmail.com>
2016-02-21 23:39:05 -0500

    Normalized whitespace

--- App/Http/Middleware/SampleMiddleware.swift
+++ App/Http/Middleware/SampleMiddleware.swift
@@ -2,21 +2,21 @@ import Vapor
 
 class SampleMiddleware: Middleware {
 
-    func handle(handler: Request -> Response) -> (Request -> Response) {
-        return { request in
-        	// You can manipulate the request before calling the handler
-        	// and abort early if necessary, a good injection point for
-        	// handling auth.
+	func handle(handler: Request -> Response) -> (Request -> Response) {
+		return { request in
+			// You can manipulate the request before calling the handler
+			// and abort early if necessary, a good injection point for
+			// handling auth.
 
-        	// return Response(status: .Forbidden, text: "Permission denied")
+			// return Response(status: .Forbidden, text: "Permission denied")
 
-            let response = handler(request)
+			let response = handler(request)
 
-            // You can also manipulate the response to add headers
-            // cookies, etc.
+			// You can also manipulate the response to add headers
+			// cookies, etc.
 
-            return response
-        }
-    }
+			return response
+		}
+	}
 
 }
