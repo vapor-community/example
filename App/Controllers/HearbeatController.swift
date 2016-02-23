5fca1184a3fe09daa3515628d2d38336b45a449e
Tanner Nelson <tanner@bluebite.com>
2016-02-23 15:07:10 -0500

    update controller

--- App/Controllers/HearbeatController.swift
+++ App/Controllers/HearbeatController.swift
@@ -2,8 +2,29 @@ import Vapor
 
 class HeartbeatController: Controller {
 
-	override func index(request: Request) throws -> ResponseConvertible {
+	/// List all resources
+	func index(request: Request) throws -> ResponseConvertible {
 		return ["lub": "dub"]
 	}
+
+	/// Create a new instance.
+    func store(request: Request) throws -> ResponseConvertible {
+        return ""
+    }
+
+    /// Show an instance.
+    func show(request: Request) throws -> ResponseConvertible {
+        return ""
+    }
+
+    /// Update an instance.
+    func update(request: Request) throws -> ResponseConvertible {
+        return ""
+    }
+
+    /// Delete an instance.
+    func destroy(request: Request) throws -> ResponseConvertible {
+        return ""
+    }
 
 }
