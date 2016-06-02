da9c2dc10f23a6168c543d49e28ce1057f286ef4
Tanner Nelson <tanner@bluebite.com>
2016-06-02 14:24:52 -0400

    update mustache

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 10),
-		.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 5)
+		.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 6)
     ],
     exclude: [
 	    "Config",
