dea4e3d633be67a1698e6132465238961c6e60ee
Logan Wright <logan.william.wright@gmail.com>
2016-09-09 12:20:12 -0400

    update vapa 18

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 17)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18)
     ],
     exclude: [
         "Config",
