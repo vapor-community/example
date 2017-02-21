5b1d631e71e881c234049a7833f97008ef99be1a
Charles A. Plater <ab3189@wayne.edu>
2017-02-21 11:29:19 -0500

    Refactored meetup API call for brevity

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -36,17 +36,9 @@ let _ = drop.config["app", "key"]?.string ?? ""
 drop.get("/") { request in
     // Get the next meetup from the meetup.com API
     let meetupResponse = try drop.client.get("https://api.meetup.com/mi-swift/events?photo-host=public&page=1&sig_id=92233812")
-    // Initialize variables
-    var attendees: Int? // The number of attendees who have RSVP'd Yes
-    var link, name: String? // A URL to the event page and the title of the event
     if let response = try JSON(bytes: meetupResponse.body.bytes!)[0] {
-        attendees = response["yes_rsvp_count"]?.node.int
-        link = response["link"]?.string
-        name = response["name"]?.string
-    }
-    if let attendees = attendees, let link = link, let name = name {
         // If we have data from the API, pass it to the template
-        return try drop.view.make("home", Node(node: ["attendees": "\(attendees)", "link": "\(link)", "name": "\(name)"]))
+        return try drop.view.make("home", Node(node: response))
     } else {
         return try drop.view.make("home")
     }
